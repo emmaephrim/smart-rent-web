c5448cae5660072aa50d0c3d861aeecb1cfa0cac
Emmanuel Ephrim <emmaephrim@gmail.com>
2026-02-09 22:52:58 +0000

    refactor(app-routing.module): sample routes added

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+// import { Routes } from '@angular/router';
+
+// const routes: Routes = [
+//   {
+//     path: '',
+//     component: MainLayoutComponent,
+//     children: [
+//       {
+//         path: 'properties',
+//         loadChildren: () =>
+//           import('./features/properties/properties.module')
+//             .then(m => m.PropertiesModule)
+//       }
+//     ]
+//   },
+//   {
+//     path: 'auth',
+//     loadChildren: () =>
+//       import('./features/auth/auth.module')
+//         .then(m => m.AuthModule)
+//   }
+// ];
