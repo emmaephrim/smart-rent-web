4fd5462ccf91b1cf5b4006b3c33e7b23fe363ec4
Emmanuel Ephrim <emmaephrim@gmail.com>
2026-02-10 19:57:42 +0000

    feat: auth ui initialised

---
+++ src/app/features/auth/login/login-component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'login-component',
+  templateUrl: './login-component.html',
+})
+export class LoginComponent {
+  //     loginForm = this.fb.group({
+  //   email: ['', [Validators.required, Validators.email]],
+  //   password: ['', Validators.required]
+  // });
+  // submit() {
+  //   if (this.loginForm.invalid) return;
+  //   // call auth service
+  // }
+}
---
+++ src/app/features/auth/register/register-component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'register-component',
+  templateUrl: './register-component.html',
+  styleUrls: ['../auth.css'],
+})
+export class RegisterComponent {}
